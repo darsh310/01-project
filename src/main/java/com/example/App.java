0d9b615fef735dea05732830ee1c46b7a968993f
darsh310 <darshanchampion96@gmail.com>
2026-04-16 19:21:28 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -7,13 +7,17 @@ import java.net.InetSocketAddress;
 public class App {
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+
         server.createContext("/", exchange -> {
-            String response = "Hello from Jenkins + ECR 🚀";
-            exchange.sendResponseHeaders(200, response.length());
-            try (OutputStream os = exchange.getResponseBody()) {
-                os.write(response.getBytes());
-            }
+            String response = "Hello from Jenkins + ECR + EKS 🚀";
+
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
         });
+
         server.start();
         System.out.println("Server started on port 8080");
     }
